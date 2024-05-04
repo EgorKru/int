b0aac94ef1c703107eaf4a1d59ef06fedd149364
kruta <krutakovegor43@gmail.com>
2024-05-05 01:30:59 +0600

    Add Allure Report

--- src/test/java/BaseClass.java
+++ src/test/java/BaseClass.java
@@ -1,16 +1,23 @@
 
     import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
-import io.github.bonigarcia.wdm.WebDriverManager;
+    import com.codeborne.selenide.logevents.SelenideLogger;
+    import io.github.bonigarcia.wdm.WebDriverManager;
 import io.github.bonigarcia.wdm.managers.ChromeDriverManager;
-import org.junit.jupiter.api.AfterEach;
+    import io.qameta.allure.selenide.AllureSelenide;
+    import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import static com.codeborne.selenide.Selenide.open;
 
     abstract public class BaseClass {
+        @BeforeEach
         public void setUp() {
+            WebDriverManager.chromedriver().setup();
+            Configuration.browser = "Chrome";
+            Configuration.browserSize = "1920x1080";
+            SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
         }
         @BeforeEach
         public void baseURL() {
--- src/test/java/MainTestScenario.java
+++ src/test/java/MainTestScenario.java
@@ -5,32 +5,31 @@ import static com.codeborne.selenide.Selenide.open;
 public class MainTestScenario extends BaseClass{
     MainPage mainPage = new MainPage();
 
-    @BeforeEach
     @Order(1)
-    @DisplayName("Базовое открытие главной страницы сайта")
+    @DisplayName("Basic Open Main Page")
     @Test
     public void checkOpenMainPage() {
         mainPage.checkOpenMainPageFromHeaders();
     }
-    @DisplayName("Переход на страницу авторизации")
+    @DisplayName("Transition In Authorisation Page")
     @Test
     public void transitionRegPage() {
         mainPage.transitionInLoginPage();
     }
-    @DisplayName("Ввод валидных данных для входа, вход в ЛК")
+    @DisplayName("Input valid data")
     @Test
     public void enterValidInf() {
         mainPage.transitionInLoginPage();
         mainPage.enterValidInfo();
     }
-    @DisplayName("Выбор пола в ЛК")
+    @DisplayName("Choice gender in Personal Area")
     @Test
     public void changesGenderCheck() {
         mainPage.transitionInLoginPage();
         mainPage.enterValidInfo();
         mainPage.setGenderSelectionManActions();
     }
-    @DisplayName ("Проверка корректности выхода из ЛК")
+    @DisplayName ("Check correct exit from Personal Area")
     @Test
     public void exitFromPersonalAccount () {
         mainPage.transitionInLoginPage();
@@ -38,7 +37,7 @@ public class MainTestScenario extends BaseClass{
         mainPage.setGenderSelectionManActions();
         mainPage.exitFromPersAccount();
     }
-    @DisplayName("Полный тест сценарий входа, выбора гендера и выхода из ЛК")
+    @DisplayName("Full test scenario entry, choice gender and exit from Personal Area")
     @Test
     public void fullScenarioMain() {
     mainPage.transitionInLoginPage();
@@ -46,4 +45,5 @@ public class MainTestScenario extends BaseClass{
     mainPage.setGenderSelectionManActions();
     mainPage.exitFromPersAccount();
     }
+    
 }
